f57caf791444ca5a15657c41ed3961c4e07388f7
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-10-24 21:47:38 +0000

    Adding new file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,12 +7,36 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp;
-	
-	temp = *list;
-	while (temp)
+	listint_t *temp_next;
+	listint_t *current;
+	listint_t *sorted;
+
+	sorted = NULL;
+	current = *list;
+	while (current)
 	{
-		printf((*list)->n);
-		temp = temp->next;
-		print_list(*list);
+		temp_next = current->next;
+		if (sorted == NULL || sorted->n >= current->n)
+		{
+			current->next = sorted;
+			current->prev = NULL;
+			if (sorted != NULL)
+				sorted->prev = current;
+			sorted = current;
+		}
+		else
+		{
+			temp = sorted;
+			while (temp->next != NULL && temp->next->n < current->n)
+				temp = temp->next;
+
+			current->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = current;
+			temp->next = current;
+			current->prev = temp;
+		}
+		current = temp_next;
 	}
+	*list = sorted;
 }
